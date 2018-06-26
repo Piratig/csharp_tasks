94dc4b73ba2474ae8f3fcc3fe19be21f3748bd4e
Piratig <his1994@mail.ru>
2018-06-26 15:35:29 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dz_1_1
+{
+    class Program
+    {             
+        static void Main(string[] args)
+        {
+            int number = 12345;
+            for (int i = 10000; i >= 1; i /= 10)
+            {
+                int outputNumber = number / i;
+                number -= outputNumber * i;
+                Console.WriteLine(outputNumber);
+            }
+            Console.ReadLine();
+        } 
+    }
+}
