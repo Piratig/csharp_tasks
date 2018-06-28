0f2ea02bf330161abb3c990f526474fe09e89a44
Kharkovskiy IS <his1994@mail.ru>
2018-06-28 15:13:25 +0300

    Задача 1_7

---
+++ Dz_1_1/Dz_1_7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitsNet;
+
+namespace Dz_1_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число в метрах = ");
+            string str = Console.ReadLine();
+            Double.TryParse(str, out double length);
+            Length meter = Length.FromMeters(length);
+            double kilometers = meter.Kilometers;
+            Console.WriteLine("В километрах = {0}км.", kilometers);
+            Console.ReadLine();
+        }
+    }
+}
